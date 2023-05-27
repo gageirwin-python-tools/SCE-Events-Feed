7197038263528a22ceadd2f0750d9feda89ec14f
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-05-27 15:09:27 -0500

    Update event_rss.py
    use Embed.from_dict() instead of self assembling embed with functions.

--- event_rss.py
+++ event_rss.py
@@ -27,20 +27,26 @@ def main():
             past_events = f.read().splitlines()
 
     webhook = SyncWebhook.from_url(webhook_url)
-    webhook.edit(name="Steam Card Exchange Events RSS")
     rss_url = "https://www.steamcardexchange.net/include/rss/events.xml"
 
     response = requests.get(rss_url)
 
     if not response.ok:
-        embed = Embed(
-            title="Error Getting Steam Events RSS",
-            url=rss_url,
-            description=f"{response.status_code} {response.reason}",
-            color=RED,
-            timestamp=datetime.now(),
+        embed_dict = {
+            "author": {
+                "name": f"Steam Card Exchange Events RSS",
+                "url": "https://www.steamcardexchange.net/include/rss/events.xml",
+                "icon_url": "https://www.steamcardexchange.net/favicon-256x256.png",
+            },
+            "title": f"Error: {response.status_code} {response.reason}",
+            "url": rss_url,
+            "color": RED,
+        }
+        webhook.send(
+            username="SCE RSS",
+            avatar_url="https://www.steamcardexchange.net/favicon-256x256.png",
+            embed=Embed.from_dict(embed_dict),
         )
-        webhook.send(embed=embed)
         quit()
 
     root = ElementTree.fromstring(response.text)
@@ -50,30 +56,47 @@ def main():
         title = item.find("title").text
         link = item.find("link").text
         appid = link[59:]
-        date = item.find("pubDate").text
         banner_url = item.find("media:content", namespace).attrib["url"]
 
         if title in past_events:
             continue
 
-        embed = Embed(
-            title=title,
-            color=GREEN,
-            timestamp=datetime.now(),
+        fields = []
+        fields.append(
+            {
+                "name": "Release Date:",
+                "value": item.find("pubDate").text,
+                "inline": False,
+            }
         )
-        embed.add_field(
-            name="Release Date:",
-            value=date,
-            inline=False,
+        fields.append(
+            {
+                "name": "Links:",
+                "value": f"[**Steam Store**](https://store.steampowered.com/app/{appid})\n[**Game Hub**](https://steamcommunity.com/app/{appid})\n[**Point Shop**](https://store.steampowered.com/points/shop/app/{appid})\n[**Steam Market**](https://steamcommunity.com/market/search?appid=753&category_753_Game%5B%5D=tag_app_{appid})",
+                "inline": False,
+            }
         )
-        embed.add_field(
-            name="Links:",
-            value=f"[**Steam Store**](https://store.steampowered.com/app/{appid})\n[**Game Hub**](https://steamcommunity.com/app/{appid})\n[**Point Shop**](https://store.steampowered.com/points/shop/app/{appid})\n[**Steam Market**](https://steamcommunity.com/market/search?appid=753&category_753_Game%5B%5D=tag_app_{appid})\n[**Steam Card Exchange**]({link})",
-            inline=False,
+
+        embed_dict = {
+            "author": {
+                "name": f"Steam Card Exchange Events RSS",
+                "url": "https://www.steamcardexchange.net/include/rss/events.xml",
+                "icon_url": "https://www.steamcardexchange.net/favicon-256x256.png",
+            },
+            "title": title,
+            "url": link,
+            "color": GREEN,
+            "fields": fields,
+            "thumbnail": {"url": banner_url},
+            "image": {"url": banner_url},
+            "timestamp": datetime.utcnow().isoformat(),
+        }
+
+        webhook.send(
+            username="SCE RSS",
+            avatar_url="https://www.steamcardexchange.net/favicon-256x256.png",
+            embed=Embed.from_dict(embed_dict),
         )
-        embed.set_thumbnail(url=banner_url)
-        embed.set_image(url=banner_url)
-        webhook.send(embed=embed)
 
         with open(ARCHIVE_FILE, "a") as f:
             f.write(f"{title}\n")
