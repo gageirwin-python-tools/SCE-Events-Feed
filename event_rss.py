02f4ddb7f9a49e268be6c515223e94996805cabb
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-05-28 00:11:34 -0500

    Update event_rss.py
    added --indefinitely argument to have the application check the rss feed indefinitely.

--- event_rss.py
+++ event_rss.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 from xml.etree import ElementTree
 
 import requests
@@ -14,94 +15,111 @@ GREEN = 0x00FF00
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--indefinitely",
+        action="store_true",
+        help="Indefinitely run the application and check www.steamcardexchange.net Events RSS feed one minute after it is updated. (feed updates every hour on the hour)",
+    )
     parser.add_argument("webhook_url", type=str, help="Discord Webhook")
-    args = parser.parse_args()
-    return args.webhook_url
+    return parser.parse_args()
 
 
 def main():
-    webhook_url = parse_arguments()
+    args = parse_arguments()
     webhook_username = "SCE Event Feed"
     sce_favicon = "https://www.steamcardexchange.net/favicon-256x256.png"
-    past_events = []
-    if os.path.exists(ARCHIVE_FILE):
-        with open(ARCHIVE_FILE, "r") as f:
-            past_events = f.read().splitlines()
 
-    webhook = SyncWebhook.from_url(webhook_url)
-    rss_url = "https://www.steamcardexchange.net/include/rss/events.xml"
+    while True:
+        past_events = []
+        if os.path.exists(ARCHIVE_FILE):
+            with open(ARCHIVE_FILE, "r") as f:
+                past_events = f.read().splitlines()
 
-    response = requests.get(rss_url)
+        webhook = SyncWebhook.from_url(args.webhook_url)
+        rss_url = "https://www.steamcardexchange.net/include/rss/events.xml"
 
-    if not response.ok:
-        embed_dict = {
-            "author": {
-                "name": webhook_username,
-                "url": rss_url,
-                "icon_url": sce_favicon,
-            },
-            "title": f"Error: {response.status_code} | {response.reason}",
-            "color": RED,
-        }
-        webhook.send(
-            username=webhook_username,
-            avatar_url=sce_favicon,
-            embed=Embed.from_dict(embed_dict),
-        )
-        quit()
+        response = requests.get(rss_url)
 
-    root = ElementTree.fromstring(response.text)
-    channel = root.find("channel")
-    feed_title = channel.find("title").text
-    feed_link = channel.find("link").text
-    feed_date = channel.find("pubDate").text
-    namespace = {"media": "http://search.yahoo.com/mrss/"}
-
-    for item in reversed(list(channel.findall("item"))):
-        title = item.find("title").text
-        link = item.find("link").text
-        appid = link[59:]
-        banner_url = item.find("media:content", namespace).attrib["url"]
-
-        if title in past_events:
-            continue
-
-        embed_dict = {
-            "author": {
-                "name": feed_title,
-                "url": feed_link,
-                "icon_url": sce_favicon,
-            },
-            "title": title,
-            "url": link,
-            "color": GREEN,
-            "fields": [
-                {
-                    "name": "Release Date:",
-                    "value": item.find("pubDate").text,
-                    "inline": False,
+        if not response.ok:
+            embed_dict = {
+                "author": {
+                    "name": webhook_username,
+                    "url": rss_url,
+                    "icon_url": sce_favicon,
                 },
-                {
-                    "name": "Links:",
-                    "value": f"[**Steam Store**](https://store.steampowered.com/app/{appid})\n[**Game Hub**](https://steamcommunity.com/app/{appid})\n[**Point Shop**](https://store.steampowered.com/points/shop/app/{appid})\n[**Steam Market**](https://steamcommunity.com/market/search?appid=753&category_753_Game%5B%5D=tag_app_{appid})",
-                    "inline": False,
+                "title": f"Error: {response.status_code} | {response.reason}",
+                "color": RED,
+            }
+            webhook.send(
+                username=webhook_username,
+                avatar_url=sce_favicon,
+                embed=Embed.from_dict(embed_dict),
+            )
+            quit()
+
+        root = ElementTree.fromstring(response.text)
+        channel = root.find("channel")
+        feed_title = channel.find("title").text
+        feed_link = channel.find("link").text
+        feed_date = channel.find("pubDate").text
+
+        for item in reversed(list(channel.findall("item"))):
+            title = item.find("title").text
+            link = item.find("link").text
+            appid = link[59:]
+            banner_url = item.find(
+                "media:content", {"media": "http://search.yahoo.com/mrss/"}
+            ).attrib["url"]
+
+            if title in past_events:
+                continue
+
+            embed_dict = {
+                "author": {
+                    "name": feed_title,
+                    "url": feed_link,
+                    "icon_url": sce_favicon,
                 },
-            ],
-            "thumbnail": {"url": banner_url},
-            "image": {"url": banner_url},
-            "timestamp": datetime.strptime(
-                feed_date, "%a, %d %b %Y %H:%M:%S %Z"
-            ).isoformat(),
-        }
+                "title": title,
+                "url": link,
+                "color": GREEN,
+                "fields": [
+                    {
+                        "name": "Release Date:",
+                        "value": item.find("pubDate").text,
+                        "inline": False,
+                    },
+                    {
+                        "name": "Links:",
+                        "value": f"[**Steam Store**](https://store.steampowered.com/app/{appid})\n[**Game Hub**](https://steamcommunity.com/app/{appid})\n[**Point Shop**](https://store.steampowered.com/points/shop/app/{appid})\n[**Steam Market**](https://steamcommunity.com/market/search?appid=753&category_753_Game%5B%5D=tag_app_{appid})",
+                        "inline": False,
+                    },
+                ],
+                "thumbnail": {"url": banner_url},
+                "image": {"url": banner_url},
+                "timestamp": datetime.strptime(
+                    feed_date, "%a, %d %b %Y %H:%M:%S %Z"
+                ).isoformat(),
+            }
 
-        webhook.send(
-            username=webhook_username,
-            avatar_url=sce_favicon,
-            embed=Embed.from_dict(embed_dict),
-        )
+            webhook.send(
+                username=webhook_username,
+                avatar_url=sce_favicon,
+                embed=Embed.from_dict(embed_dict),
+            )
 
-        with open(ARCHIVE_FILE, "a") as f:
-            f.write(f"{title}\n")
+            with open(ARCHIVE_FILE, "a") as f:
+                f.write(f"{title}\n")
+
+        if args.indefinitely == False:
+            break
+
+        last_updated = datetime.strptime(feed_date, "%a, %d %b %Y %H:%M:%S %Z")
+        next_update = last_updated + timedelta(hours=1, minutes=1)
+        dt = (next_update - datetime.utcnow()).total_seconds()
+        sleep = dt if dt > 0 else 0
+        print(f"Waiting for next update in {sleep} seconds.")
+        time.sleep(sleep)
 
 
 if __name__ == "__main__":
