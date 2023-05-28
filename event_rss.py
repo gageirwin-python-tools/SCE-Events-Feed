a35dd339afddf78ff02a0284f6671de01a399fa5
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-05-27 23:25:34 -0500

    Update event_rss.py

--- event_rss.py
+++ event_rss.py
@@ -21,8 +21,8 @@ def parse_arguments():
 
 def main():
     webhook_url = parse_arguments()
-    webhook_username = "SCE RSS"
-    webhook_avatar_url = "https://www.steamcardexchange.net/favicon-256x256.png"
+    webhook_username = "SCE Event Feed"
+    sce_favicon = "https://www.steamcardexchange.net/favicon-256x256.png"
     past_events = []
     if os.path.exists(ARCHIVE_FILE):
         with open(ARCHIVE_FILE, "r") as f:
@@ -36,25 +36,28 @@ def main():
     if not response.ok:
         embed_dict = {
             "author": {
-                "name": f"Steam Card Exchange Events RSS",
+                "name": webhook_username,
                 "url": rss_url,
-                "icon_url": "https://www.steamcardexchange.net/favicon-256x256.png",
+                "icon_url": sce_favicon,
             },
-            "title": f"Error: {response.status_code} {response.reason}",
-            "url": rss_url,
+            "title": f"Error: {response.status_code} | {response.reason}",
             "color": RED,
         }
         webhook.send(
             username=webhook_username,
-            avatar_url=webhook_avatar_url,
+            avatar_url=sce_favicon,
             embed=Embed.from_dict(embed_dict),
         )
         quit()
 
     root = ElementTree.fromstring(response.text)
+    channel = root.find("channel")
+    feed_title = channel.find("title").text
+    feed_link = channel.find("link").text
+    feed_date = channel.find("pubDate").text
     namespace = {"media": "http://search.yahoo.com/mrss/"}
 
-    for item in reversed(list(root.iter("item"))):
+    for item in reversed(list(channel.findall("item"))):
         title = item.find("title").text
         link = item.find("link").text
         appid = link[59:]
@@ -65,9 +68,9 @@ def main():
 
         embed_dict = {
             "author": {
-                "name": f"Steam Card Exchange Events RSS",
-                "url": rss_url,
-                "icon_url": "https://www.steamcardexchange.net/favicon-256x256.png",
+                "name": feed_title,
+                "url": feed_link,
+                "icon_url": sce_favicon,
             },
             "title": title,
             "url": link,
@@ -86,12 +89,14 @@ def main():
             ],
             "thumbnail": {"url": banner_url},
             "image": {"url": banner_url},
-            "timestamp": datetime.utcnow().isoformat(),
+            "timestamp": datetime.strptime(
+                feed_date, "%a, %d %b %Y %H:%M:%S %Z"
+            ).isoformat(),
         }
 
         webhook.send(
             username=webhook_username,
-            avatar_url=webhook_avatar_url,
+            avatar_url=sce_favicon,
             embed=Embed.from_dict(embed_dict),
         )
 
